5b773c6a6bad25a8ad715719f287b8b428d5b3cf
@eduardo <jose.eduardo@inbec.edu.br>
2023-07-12 08:21:34 -0300

    get contact data

--- scrapin_linkedin.py
+++ scrapin_linkedin.py
@@ -50,6 +50,16 @@ profiles_button.click()
 print("Busca por perfis rezalizada com sucesso")
 sleep(20)
 
+# 2.5 Search by company
+# company_button = browser.find_element(By.XPATH, '//button[text()="Empresa atual"]')
+# company_button.click()
+# sleep(10)
+# input_company = browser.find_element(By.CSS_SELECTOR, '[aria-label="Adicionar empresa"]')
+# input_company.send_keys('petrobras')
+# input_area = browser.find_element(By.CLASS_NAME, 'search-basic-typeahead')
+# print(input_area.get_attribute('outerHTML'))
+
+
 # 3 Scrape the URLs of the profiles
 
 # 3.1 Function to extract the URLs of one page
@@ -73,7 +83,7 @@ def GetUrl():
     return all_profile_URL
 
 # 3.2 Navigate through many page, and extract the profile URLs of each page
-number_of_pages = 3
+number_of_pages = 1
 URLs_all_page = []
 for page in range(number_of_pages):
     URLs_one_page = GetUrl()
@@ -90,17 +100,58 @@ print("Todas as URLS dos perfis foram obtidas")
 # 4 Scrape the data of 1 Linkedin profile, and write the data to a .CSV file
 list_data_profiles = []
 for linkedin_URL in URLs_all_page:
+    data_one_profile = []
     browser.get(linkedin_URL)
+    print()
     print('Acessando perfil: ', linkedin_URL)
     sleep(random.randint(2, 5))
 
     profile_page = BeautifulSoup(browser.page_source, "html.parser")
 
+    # name
     name = profile_page.find('h1', attrs={'class', 'text-heading-xlarge inline t-24 v-align-middle break-words'})
     if name is None:
         print('Não foi possível acessar a url do perfil')
         continue
-    # 4.1 Get formations elements
+    data_one_profile.append(name.text)
+
+    # contact
+    contact_button = browser.find_element(By.ID, 'top-card-text-details-contact-info')
+    contact_button.click()
+    sleep(random.randint(4, 7))
+    data_contact_modal = BeautifulSoup(browser.page_source, "html.parser")
+    data_contact_sections = data_contact_modal.find_all('section', attrs={'class': 'pv-contact-info__contact-type'})
+    all_data_contact = []
+    for data_contact_element in data_contact_sections:
+        title = data_contact_element.find('h3').text.strip()
+
+        content = []
+
+        div_info = data_contact_element.find('div', attrs={'class': 'pv-contact-info__ci-container'})
+        if div_info is not None:
+            contact_link = div_info.find('a')
+            if contact_link is not None:
+                contact_href = contact_link.get('href').strip()
+                if contact_href is not None:
+                    content.append(contact_href)
+            else:
+                data_span = div_info.find('span').text.strip()
+                content.append(data_span)
+
+        ul_infos = data_contact_element.find('ul')
+        if (ul_infos is not None):
+            links_contact_list = ul_infos.find_all('li')
+            for li in links_contact_list:
+               contact_link = li.find('a')['href']
+               content.append(contact_link)
+        content_string = ", ".join(content)
+        data_string = ": ".join([title, content_string])
+        all_data_contact.append(data_string)
+    data_one_profile.append("| ".join(all_data_contact))
+    browser.back()
+    sleep(random.randint(4, 7))
+
+    # formations
     try:
         see_all_education_button = browser.find_element(By.ID, 'navigation-index-see-all-education')
         print('Botão de ver todas a formações encontrado')
@@ -114,7 +165,6 @@ for linkedin_URL in URLs_all_page:
         div_education = profile_page.find('div', {'id': 'education'})
         section_education = div_education.find_parent('section')
         formations_list = section_education.find_all('div', attrs={'class': 'pvs-entity'})
-    # 4.2 Get formations list
     formations = []
     for formation_list_item in formations_list:
         institution = formation_list_item.find('span', attrs={'aria-hidden': 'true'})
@@ -125,7 +175,9 @@ for linkedin_URL in URLs_all_page:
         else:
             formations.append(institution.text)
     formations_string = "| ".join(formations)
-    # 4.3 Get certifications elements
+    data_one_profile.append(formations_string)
+
+    # certifications
     try:
         see_all_certifications_button = browser.find_element(By.ID, 'navigation-index-see-all-licenses-and-certifications')
         print('Botão de ver todas a certificações encontrado')
@@ -150,13 +202,14 @@ for linkedin_URL in URLs_all_page:
         certifications.append(certification_name.text + ' ' + '-' + ' ' + entity.text)
     if (len(certifications) > 0):
         certifications_string = "| ".join(certifications)
-        list_data_profiles.append([name.text, formations_string, certifications_string])
+        data_one_profile.append(certifications_string)
     else:
-        list_data_profiles.append([name.text, formations_string, ' '])
+        data_one_profile.append(' ')
+    list_data_profiles.append(data_one_profile)
     browser.execute_script('window.scrollTo(0, document.body.scrollHeight);') #scroll to the end of the page
 
 # 5 Write the data to a .xlsx file
-data_profiles = pd.DataFrame(list_data_profiles, columns=['Nome', 'Formação Acadêmica', 'Certificações'])
+data_profiles = pd.DataFrame(list_data_profiles, columns=['Nome', 'Contato', 'Formação Acadêmica', 'Certificações'])
 
 data_profiles.to_excel('dados.xlsx', index=False)
 print("Arquivo xlsx criado com sucesso")
